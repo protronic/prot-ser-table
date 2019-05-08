a65239194facedb0629d3d94675b9ffd5c8556de
Robert Seidler <r.seidler@protronic-gmbh.de>
2019-05-08 14:56:54 +0200

    implemented couch config load

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <prot-table :data_url='data_url' :table_options="options"></prot-table>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue';
+import Vue from 'vue';
+import PouchDB from 'pouchdb-browser';
+import ProtTable from 'prot-table';
+
+let pouch = new PouchDB('http://prot-subuntu:5985/prot-table');
+
+let url = new URL(location.href)
+let query_string = url.search;
+let search_params = new URLSearchParams(query_string);
+
+let page = search_params.get('page');
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    ProtTable
+  },
+  mounted(){
+    pouch.get(page)
+      .then( response => {
+        console.log(response);
+        this.options = response;
+      });
+  },
+  data(){
+    return {
+      data_url: 'data.json',
+      options: {},
+    };
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  /* font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 60px; */
 }
 </style>
